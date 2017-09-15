72ca909b0de249aa568a88db3d571db5f4c6010c
Brodie Gilmour <brodie.gilmour@webappuk.com>
2017-09-15 16:49:44 +0100

    Adding in the classes model, and linking the classes a particular client in assigned to.

---
+++ Gyms/Models/Class.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using NodaTime;
+
+namespace Gyms.Models
+{
+    public class Class
+    {
+        [Column("Duration")]
+        private double _durationTicks;
+
+        public int ID { get; set; }
+        public Instructor Instructor { get; set; }
+
+        [NotMapped]
+        public List<Client> Clients { get; set; }
+
+        public DateTime Date { get; set; }
+
+        [NotMapped]
+        public Duration Duration
+        {
+            get => Duration.FromTicks(_durationTicks);
+            set => _durationTicks = value.TotalTicks;
+        }
+
+        public Class()
+        {
+            this.Clients = new List<Client>();
+        }
+    }
+}
--- Gyms/Models/Client.cs
+++ Gyms/Models/Client.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,18 @@ namespace Gyms.Models
 {
     public class Client
     {
+
         public int ID { get; set; }
         public string Name { get; set; }
         public DateTime DateOfBirth { get; set; }
+
+        [NotMapped]
+        public List<Class> Classes { get; set; }
+
+        public Client()
+        {
+            this.Classes = new List<Class>();
+        }
 
     }
 }
