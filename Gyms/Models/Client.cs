68b1ef20e9191483632f0c80e89eff78f2ce2bac
Brodie Gilmour <brodie.gilmour@webappuk.com>
2017-09-22 15:33:18 +0100

    Adding the Display name attribute for some properties. Adding documentation.

--- Gyms/Models/Client.cs
+++ Gyms/Models/Client.cs
@@ -1,21 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Gyms.Models
 {
     public class Client
     {
-
         public int ID { get; set; }
+
+        /// <summary>
+        /// Gets or sets the first name.
+        /// </summary>
+        /// <value>
+        /// The first name.
+        /// </value>
+        [DisplayName("First Name")]
         public string FirstName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the surname.
+        /// </summary>
+        /// <value>
+        /// The surname.
+        /// </value>
         public string Surname { get; set; }
 
+        /// <summary>
+        /// Gets or sets the date of birth.
+        /// </summary>
+        /// <value>
+        /// The date of birth.
+        /// </value>
+        [DisplayName("Date of Birth")]
         public DateTime DateOfBirth { get; set; }
 
+        /// <summary>
+        /// Gets or sets the classes.
+        /// </summary>
+        /// <value>
+        /// The classes.
+        /// </value>
         [NotMapped]
         public List<Class> Classes { get; set; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Client"/> class.
+        /// </summary>
         public Client()
         {
             this.Classes = new List<Class>();
--- Gyms/Models/Instructor.cs
+++ Gyms/Models/Instructor.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace Gyms.Models
 {
     public class Instructor
     {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>
+        /// The identifier.
+        /// </value>
         public int ID { get; set; }
+
+        /// <summary>
+        /// Gets or sets the first name.
+        /// </summary>
+        /// <value>
+        /// The first name.
+        /// </value>
+        [DisplayName("First Name")]
         public string FirstName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the surname.
+        /// </summary>
+        /// <value>
+        /// The surname.
+        /// </value>
         public string Surname { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date of birth.
+        /// </summary>
+        /// <value>
+        /// The date of birth.
+        /// </value>
+        [DisplayName("Date of Birth")]
         public DateTime DateOfBirth { get; set; }
     }
 }
